bb203d914db958be3fcbbe2abba12359e7ff5bcd
Bashar <bashikr@gmail.com>
2021-09-22 15:33:49 +0200

    documents.test.js

--- test/documents.test.js
+++ test/documents.test.js
@@ -3,26 +3,53 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
+const createConnection = require("../connection-mongodb/db.js");
 
 chai.should();
 chai.use(chaiHttp);
 
-let id = '';
+var collectionName= "docs";
+var db;
+var id = '';
+
+
 
 describe('Test the functionality of documents API', () => {
-    // describe('GET /documents', () => {
-    //     it('400 SAD PATH (Throws an error when the documents collection is empty)', (done) => {
-    //         chai.request(server)
-    //             .get("/documents")
-    //             .end((err, res) => {
-    //                 res.should.have.status(400);
-    //                 res.body.should.be.an("object");
-    //                 res.body.should.have.property('error');
-    //                 res.body.should.have.property('error').eq('Document collection is empty');
-    //                 done();
-    //             });
-    //     });
-    // });
+    before(() => {
+        async () => {
+            db = await createConnection(collectionName);
+
+            db.db.listCollections(
+                { name: collectionName }
+            )
+                .next()
+                .then(async function (info) {
+                    if (info) {
+                        await db.collection.drop();
+                    }
+                })
+                .catch(function (err) {
+                    console.error(err);
+                })
+                .finally(async function () {
+                    await db.client.close();
+                });
+        };
+    });
+
+    describe('GET /documents', () => {
+        it('400 SAD PATH (Throws an error when the documents collection is empty)', (done) => {
+            chai.request(server)
+                .get("/documents")
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property('error');
+                    res.body.should.have.property('error').eq('Document collection is empty');
+                    done();
+                });
+        });
+    });
 
     describe('POST /documents/create-doc', () => {
         it('201 HAPPY PATH', (done) => {
@@ -39,7 +66,6 @@ describe('Test the functionality of documents API', () => {
                 .end((err, res) => {
                     res.should.have.status(201);
                     res.body.should.be.an("object");
-
                     done();
                 });
         });
@@ -61,132 +87,125 @@ describe('Test the functionality of documents API', () => {
                     res.body.should.have.property('error');
                     res.body.should.have.property('error')
                         .eq('Title and content should be of type string');
+                    done();
+                });
+        });
+    });
+
+    describe('GET /documents', () => {
+        it('200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/documents")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("array");
+                    res.body.length.should.be.above(0);
+                    for (let item in res.body) {
+                        res.body[item].should.be.an("object");
+                        id = res.body[0]._id;
+                    }
 
                     done();
                 });
         });
     });
 
-    // describe('GET /documents', () => {
-    //     it('200 HAPPY PATH', (done) => {
-    //         chai.request(server)
-    //             .get("/documents")
-    //             .end((err, res) => {
-    //                 res.should.have.status(200);
-    //                 res.body.should.be.an("array");
-    //                 res.body.length.should.be.above(0);
-    //                 for (let item in res.body) {
-    //                     res.body[item].should.be.an("object");
-    //                     id = res.body[0]._id;
-    //                 }
+    describe('GET /documents/:id', () => {
+        it('200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .get("/documents/" + id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("_id");
+                    res.body.should.have.property("title");
+                    res.body.should.have.property("content");
+                    res.body.should.have.property("creationDate");
+                    done();
+                });
+        });
+        it('404 SAD PATH (Requested document is not found)', (done) => {
+            let fakeId = "123456789123";
 
-    //                 done();
-    //             });
-    //     });
-    // });
+            chai.request(server)
+                .get("/documents/" + fakeId)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property('error');
+                    res.body.should.have.property('error').eq('Requested document is not found');
+                    done();
+                });
+        });
+    });
 
-    // describe('GET /documents/:id', () => {
-    //     it('200 HAPPY PATH', (done) => {
-    //         chai.request(server)
-    //             .get("/documents/" + id)
-    //             .end((err, res) => {
-    //                 res.should.have.status(200);
-    //                 res.body.should.be.an("object");
-    //                 res.body.should.have.property("_id");
-    //                 res.body.should.have.property("title");
-    //                 res.body.should.have.property("content");
-    //                 res.body.should.have.property("creationDate");
+    describe('PUT /documents/update-doc', () => {
+        it('201 HAPPY PATH', (done) => {
+            let updateDocument = {
+                '_method': 'put',
+                '_id': id,
+                'title': 'Test update document',
+                'content': 'update content',
+                'updateDate': new Date()
+            };
 
-    //                 done();
-    //             });
-    //     });
-    //     it('404 SAD PATH (Requested document is not found)', (done) => {
-    //         let fakeId = "123456789123";
+            chai.request(server)
+                .put("/documents/update-doc")
+                .send(updateDocument)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.body.should.be.an("object");
+                    done();
+                });
+        });
+        var invalidTitle = 1;
+        var invalidContent = 1;
 
-    //         chai.request(server)
-    //             .get("/documents/" + fakeId)
-    //             .end((err, res) => {
-    //                 res.should.have.status(404);
-    //                 res.body.should.be.an("object");
-    //                 res.body.should.have.property('error');
-    //                 res.body.should.have.property('error').eq('Requested document is not found');
+        it('400 SAD PATH (title and content not of type string)', (done) => {
+            let updateDocument = {
+                '_method': 'put',
+                '_id': id,
+                'title': invalidTitle,
+                'content': invalidContent,
+                'updateDate': new Date()
+            };
 
-    //                 done();
-    //             });
-    //     });
-    // });
-
-    // describe('PUT /documents/update-doc', () => {
-    //     it('201 HAPPY PATH', (done) => {
-    //         let updateDocument = {
-    //             '_method': 'put',
-    //             '_id': id,
-    //             'title': 'Test update document',
-    //             'content': 'update content',
-    //             'updateDate': new Date()
-    //         };
-
-    //         chai.request(server)
-    //             .put("/documents/update-doc")
-    //             .send(updateDocument)
-    //             .end((err, res) => {
-    //                 res.should.have.status(201);
-    //                 res.body.should.be.an("object");
-
-    //                 done();
-    //             });
-    //     });
-    //     var invalidTitle = 1;
-    //     var invalidContent = 1;
-
-    //     it('400 SAD PATH (title and content not of type string)', (done) => {
-    //         let updateDocument = {
-    //             '_method': 'put',
-    //             '_id': id,
-    //             'title': invalidTitle,
-    //             'content': invalidContent,
-    //             'updateDate': new Date()
-    //         };
-
-    //         chai.request(server)
-    //             .put("/documents/update-doc")
-    //             .send(updateDocument)
-    //             .end((err, res) => {
-    //                 res.should.have.status(400);
-    //                 res.body.should.be.an("object");
-    //                 res.body.should.have.property('error');
-    //                 res.body.should.have.property('error')
-    //                     .eq('Title and content should be of type string');
-
-    //                 done();
-    //             });
-    //     });
-    // });
+            chai.request(server)
+                .put("/documents/update-doc")
+                .send(updateDocument)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property('error');
+                    res.body.should.have.property('error')
+                        .eq('Title and content should be of type string');
+                    done();
+                });
+        });
+    });
 
     describe('DELETE /documents/delete-doc', () => {
-        // it('200 HAPPY PATH', (done) => {
-        //     chai.request(server)
-        //         .delete("/documents/delete-doc/" + id)
-        //         .end((err, res) => {
-        //             res.should.have.status(200);
+        it('200 HAPPY PATH', (done) => {
+            chai.request(server)
+                .delete("/documents/delete-doc/" + id)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
 
-        //             done();
-        //         });
-        // });
+        it('404 SAD PATH', (done) => {
+            let fakeId = "123456789123";
 
-        // it('404 SAD PATH', (done) => {
-        //     let fakeId = "123456789123";
-
-        //     chai.request(server)
-        //         .delete("/documents/delete-doc/" + fakeId)
-        //         .end((err, res) => {
-        //             res.should.have.status(404);
-        //             res.body.should.have.property('error');
-        //             res.body.should.have.property('error')
-        //                 .eq('The given document id is not found');
-
-        //             done();
-        //         });
-        // });
+            chai.request(server)
+                .delete("/documents/delete-doc/" + fakeId)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.have.property('error');
+                    res.body.should.have.property('error')
+                        .eq('The given document id is not found');
+                    done();
+                });
+        });
     });
 });
