2897adcf69c842be58971787337f6542d31bd76f
Bashar <bashikr@gmail.com>
2021-10-12 21:30:17 +0200

    Connected socket.io to magicEditor's node server

--- app.js
+++ app.js
@@ -13,21 +13,48 @@ const middleWare = require('./middleware/error.handler.js');
 const index = require('./routes/index.route.js');
 const documents = require('./routes/documents.route.js');
 const path = require('path');
+const httpServer = require("http").createServer(app);
 
 app.use(express.static(path.join(__dirname, '../editor-angular-frontend/dist/editor-angular')));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: ["http://localhost:4200", 'http://www.student.bth.se'],
+        methods: ["GET", "POST", "PUT"]
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.on('openDoc', function (docId) {
+        socket.join(docId);
+    });
+
+    socket.on('updateDocument', (doc) => {
+        socket.to(doc._id).emit("updateDocument", doc);
+    });
+
+    socket.on('updateContent', (object) => {
+        socket.to(object.id).emit("updateDocument", object.content);
+    });
+
+    socket.on('closeDoc', (docId) => {
+        socket.leave(docId);
+    });
+});
+
+
 app.use('/', index);
 app.use('/documents', documents);
-
 app.use(middleWare.middleWare), app.use(middleWare.notFoundError), app.use(middleWare.errorResult);
 
 if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('combined'));
 }
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = httpServer.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = server;
